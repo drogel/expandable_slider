11d27f5e5ee43a827256d8a18659f1f6c4b08d40
diego-rogel <diegorogel3d@hotmail.com>
2020-03-05 23:46:15 +0100

    inits view model

---
+++ lib/src/view_model.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:expandable_slider/src/states.dart';
+import 'package:flutter/foundation.dart';
+
+class ExpandableSliderViewModel {
+  const ExpandableSliderViewModel({
+    @required StreamController<SliderState> stateController,
+  })  : assert(stateController != null),
+        _stateController = stateController;
+
+  final StreamController<SliderState> _stateController;
+  
+  Stream<SliderState> get stateStream => _stateController.stream;
+
+  void dispose() => _stateController.close();
+}
